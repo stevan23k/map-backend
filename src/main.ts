fab0ba4364dcdd942523e4eb85956193d2cd7a5d
stevan23k <esteproo@gmail.com>
2026-03-28 00:59:17 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', process.env.FRONTEND_URL], // Allowed origins
+    origin: ['https://map-front-bnnmel-5330c4-45-90-237-194.traefik.me', 'http://localhost:3000'], // Allowed origins
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
     credentials: true, // Allow credentials (e.g., cookies)
     allowedHeaders: 'Content-Type, Accept, Authorization', // Allowed headers
