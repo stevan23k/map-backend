ab38bd5a273831b095e0b637ce1e5221c1c7c9e0
stevan23k <esteproo@gmail.com>
2026-03-28 14:45:28 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,20 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://map-front-bnnmel-5330c4-45-90-237-194.traefik.me', 'http://localhost:3000'], // Allowed origins
+    origin: [process.env.FRONTEND_URL, 'http://localhost:3000'], // Allowed origins
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
     credentials: true, // Allow credentials (e.g., cookies)
     allowedHeaders: 'Content-Type, Accept, Authorization', // Allowed headers
   });
 
   // Add global validation pipe
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
