7c6b5ad93bb594326fc487c2f89b3a76f76a9767
stevan23k <esteproo@gmail.com>
2026-03-31 01:57:05 -0500

    dominio

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [process.env.FRONTEND_URL, 'http://localhost:3000'], // Allowed origins
+    origin: ['https://mapp.jes.com.co', 'http://localhost:3000'], // Allowed origins
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
     credentials: true, // Allow credentials (e.g., cookies)
     allowedHeaders: 'Content-Type, Accept, Authorization', // Allowed headers
