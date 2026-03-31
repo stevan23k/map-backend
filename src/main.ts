b24e5e3d88474b4f4f4202e9b3e55b8a2ae7af66
stevan23k <esteproo@gmail.com>
2026-03-31 13:19:57 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const frontendUrl = process.env.FRONTEND_URL || 'https://mapp.jes.com.co';
   app.enableCors({
-    origin: [frontendUrl, frontendUrl.replace('https://', 'http://')],
+    origin: [frontendUrl, frontendUrl.replace('https://', 'http://'), 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept, Authorization, X-Requested-With',
